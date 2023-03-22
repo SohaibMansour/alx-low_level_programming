fb6295480cc1b8e6bdfd86c51b91382be106ce8e
SohaibMansour <sohaibmansour@gmail.com>
2023-03-22 14:08:15 -0700

    a

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * main - Entry point
+ *
+ * Description: prints _putchar using putchar prototype
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char c[] = "_putchar";
+	int x;
+
+	for (x = 0; x < 9; x++)
+	{
+		_putchar(c[x]);
+	}
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_
+
+#include <stdio.h>
+
+int _putchar(char c);
+
+#endif
