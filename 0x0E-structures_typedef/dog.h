4113cb417d44fb7424c95e4c28e54da1c26f1a9f
SohaibMansour <sohaibmansour@gmail.com>
2023-04-17 16:49:53 -0700

    a

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's basic info
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ *
+ * Description: Longer dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
